e3a97a81f98287a7b608f8f73210d6c8c420588f
51348761z <hl_wong@st.gxu.edu.cn>
2026-01-22 23:41:53 +0800

    Add background overlay for movie details page

--- js/pages/movie-details.js
+++ js/pages/movie-details.js
@@ -6,6 +6,31 @@ export async function renderMovieDetails() {
   const movieId = window.location.search.split("=")[1];
   const movie = await TMDBService.fetchMovieDetailsById(movieId);
 
+  // Overlay for background image
+  displayBackgroundImage("movie", movie.backdrop_path);
+
   const movieDetailsCard = createMovieDetailsCard(movie);
   document.querySelector(".movie-details").appendChild(movieDetailsCard);
+}
+
+// Display Backdrop On Details Page
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement("div");
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = "cover";
+  overlayDiv.style.backgroundPosition = "center";
+  overlayDiv.style.backgroundRepeat = "no-repeat";
+  overlayDiv.style.width = "100vw";
+  overlayDiv.style.height = "100vh";
+  overlayDiv.style.position = "absolute";
+  overlayDiv.style.top = "0";
+  overlayDiv.style.left = "0";
+  overlayDiv.style.zIndex = "-1";
+  overlayDiv.style.opacity = "0.15";
+
+  if (type === "movie") {
+    document.querySelector(".movie-details").appendChild(overlayDiv);
+  } else {
+    document.querySelector("#show-details").appendChild(overlayDiv);
+  }
 }
